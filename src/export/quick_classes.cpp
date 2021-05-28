92d30b5a0389ab73c9254cce22683a939a268365
Madu86 <mmmskumara@gmail.com>
2021-05-28 11:25:24 -0700

    implemented functions of property class

--- src/export/quick_classes.cpp
+++ src/export/quick_classes.cpp
@@ -1,6 +1,6 @@
 /*
   !---------------------------------------------------------------------!
-  ! Written by Madu Manathunga on 05/19/2020                            !
+  ! Written by Madu Manathunga on 05/19/2021                            !
   !                                                                     !
   ! Copyright (C) 2020-2021 Merz lab                                    !
   ! Copyright (C) 2020-2021 Götz lab                                    !
@@ -19,28 +19,59 @@
 
 using namespace qk;
 
-// functions belonging to quick class
+/*
+  ---------------------------- functions belonging to quick class -----------------------------
+*/
+
+// constructor
 quick::quick(void){quick_version="";}
 
-// functions belonging to dataOperator class
+// destructor
+quick::~quick(void){}
+
+/*
+  ------------------------ functions belonging to dataOperator class --------------------------
+*/
+
 template <typename T> 
 std::vector<T>* dataOperator::Transfer(T *f90data, int length1, int length2){
 
-  std::vector<T>* vec=new std::vector<T>(length1*length2);
+  std::vector<T>* vec=NULL;
 
-  int idxf90=0;
+  if(f90data != NULL){
 
-  for(int j=0;j<length2;++j){
-    for(int i=0;i<length1;++i){
-      vec[ j * length1 + i ] = f90data[idxf90++];
+    vec = new std::vector<T>(length1*length2);
+ 
+    int idxf90=0;
+ 
+    for(int j=0;j<length2;++j){
+      for(int i=0;i<length1;++i){
+        vec->push_back(f90data[idxf90++]);
+      }
     }
+
   }
 
   return vec;
 
 }
 
-// functions belonging to molecule class
+/* get a list of symbols when a list of integers from f90 is given. Note that we correct
+   the index by deducting each index by one. */
+std::vector<std::string>* dataOperator::GetAtomSymbol(int *f90data, int length){
+
+  std::vector<std::string>* vec=new std::vector<std::string>(length);
+
+  for(int i=0; i<length; ++i){
+    vec->push_back(atom_symbols[f90data[i]-1]);
+  }  
+
+}
+
+/*
+  -------------------------- functions belonging to molecule class -------------------------------
+*/
+
 // default constructor
 molecule::molecule(void){
   molecule::natoms=0;
@@ -49,8 +80,89 @@ molecule::molecule(void){
   molecule::molecular_charge=0.0;
 }
 
+/* construct with number of atoms, atom types, cartesian coordinates, molecular charge and multiplicity.
+*/
+molecule::molecule(int natoms, int *iattype, double *coordinates, double molecular_charge){
 
-molecule::molecule(int natoms, char *atom_labels, double *coordinates, double molecular_charge){
-  
+  this->natoms=natoms; 
+  this->atom_labels=GetAtomSymbol(iattype, natoms);  
+  this->coordinates=Transfer(coordinates, natoms, 3);
+  this->molecular_charge=molecular_charge;
+
 }
 
+// destructor
+molecule::~molecule(void){
+  SAFE_DELETE(this->atom_labels);
+  SAFE_DELETE(this->coordinates);
+}
+
+/*
+  -------------------------- functions belonging to property class -------------------------------
+*/
+
+// default constructor
+property::property(void){
+  property::calc = NULL;
+  property::scf_prop = NULL;
+  property::mp2 = NULL;
+}
+
+/* construct object with only calculation information where nbasis, nmo, nalpha, nbeta stand for
+   number of basis functions, number of molecular orbitals, number of alpha and beta electrons.
+   Note that we are using constructor delegation here. */
+property::property(int nbasis, int nmo, int nalpha, int nbeta) : property::property(){
+  property::calc= new calcInfoStruct{nbasis, nmo, nalpha, nbeta};
+}
+
+/* construct object with calculation and hf information where nbasis, nmo, nalpha, nbeta stand for
+   number of basis functions, number of molecular orbitals, number of alpha and beta electrons.
+   For dipole moment, number of atoms must be also specified. */
+property::property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+                   double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_total_energy,\
+                   int scf_iterations, int natoms=0, double *scf_dipole_moment=NULL) : property::property(nbasis, \
+                   nmo, nalpha, nbeta){
+
+  property::scf_prop = new scfInfoStruct{scf_one_electron_energy, scf_two_electron_energy, nuclear_repulsion_energy,\
+                                         0.0, NULL, scf_total_energy, scf_iterations};
+  if(scf_dipole_moment != NULL){
+    property::scf_prop -> scf_dipole_moment = Transfer(scf_dipole_moment, natoms, 3);
+  }
+
+}
+
+/* construct object with calculation and dft information where nbasis, nmo, nalpha, nbeta stand for
+   number of basis functions, number of molecular orbitals, number of alpha and beta electrons.
+   For dipole moment, number of atoms must be also specified. */
+property::property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+                   double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_xc_energy,\
+                   double scf_total_energy, int scf_iterations, int natoms=0, double *scf_dipole_moment=NULL)\
+                   : property::property(nbasis, nmo, nalpha, nbeta, scf_one_electron_energy, scf_two_electron_energy,\
+                     nuclear_repulsion_energy, scf_total_energy, scf_iterations, natoms, scf_dipole_moment){
+
+  property::scf_prop -> scf_xc_energy = scf_xc_energy;
+ 
+}
+
+/* construct object with calculation and mp2 information where nbasis, nmo, nalpha, nbeta stand for
+   number of basis functions, number of molecular orbitals, number of alpha and beta electrons.
+   For dipole moment, number of atoms must be also specified. */
+property::property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+                   double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_total_energy, \
+                   int scf_iterations, double mp2_correlation_energy, int natoms=0, double *scf_dipole_moment=NULL)\
+                   : property::property(nbasis, nmo, nalpha, nbeta, scf_one_electron_energy, scf_two_electron_energy,\
+                     nuclear_repulsion_energy, scf_total_energy, scf_iterations, natoms, scf_dipole_moment){
+
+  property::mp2 = new mp2InfoStruct{mp2_correlation_energy, (mp2_correlation_energy+scf_total_energy)};
+
+}
+
+// destructor
+property::~property(){
+
+  SAFE_DELETE(property::calc);
+  SAFE_DELETE(property::scf_prop -> scf_dipole_moment);  
+  SAFE_DELETE(property::scf_prop);
+  SAFE_DELETE(property::mp2);
+
+}
--- src/export/quick_classes.h
+++ src/export/quick_classes.h
@@ -18,8 +18,12 @@
   !---------------------------------------------------------------------!
 */
 
+#ifndef QUICK_CLASSES_H
+#define QUICK_CLASSES_H
 #include <string>
 #include <vector>
+#include "quick_common.h"
+#include "quick_constants.h"
 
 namespace qk{
 
@@ -41,10 +45,12 @@ class quick{
 class dataOperator{
 
   public:
-    // templates to transfer data from f90 arrays into C++ vectors
+    // template to transfer data from f90 arrays into C++ vectors
     template <typename T> 
     static std::vector<T>* Transfer(T *f90data, int length1=1, int length2=1);
     
+    // get a list of string/char symbols when a list of integers from f90 array is given
+    static std::vector<std::string>* GetAtomSymbol(int *f90data, int length);
 };
 
 
@@ -57,10 +63,11 @@ class molecule: public dataOperator{
     std::vector<std::string> *atom_labels;   // list of atomic symbols
     std::vector<double> *coordinates;        // vector of cartesian coordinates, (3*natoms)
     double molecular_charge;                 // total charge of the molecule
+    double molecular_multiplicity;           // the overall multiplicity of molecule
 
     // constructors
     molecule(void);
-    molecule(int natoms, char *atom_labels, double *coordinates, double molecular_charge);
+    molecule(int natoms, int *iattype, double *coordinates, double molecular_charge);
 
     // destructor
     ~molecule(void);
@@ -68,5 +75,68 @@ class molecule: public dataOperator{
 };
 
 
+// structure tags required for properties class
+// calculation information
+struct calcInfoStruct{
+  int nbasis;
+  int nmo;
+  int nalpha;
+  int nbeta;
+};
+
+// hf/dft scf information
+struct scfInfoStruct{
+  double scf_one_electron_energy;
+  double scf_two_electron_energy;
+  double nuclear_repulsion_energy;
+  double scf_xc_energy;
+  std::vector<double> *scf_dipole_moment;
+  double scf_total_energy;
+  int scf_iterations;
+};
+
+// mp2 information
+struct mp2InfoStruct{
+  double mp2_correlation_energy;
+  double mp2_total_energy;  
+};
+
+// class for valid quantum chemistry properties
+class property: public dataOperator{
+  public:
+    // fields
+    calcInfoStruct *calc;
+    scfInfoStruct *scf_prop;
+    mp2InfoStruct *mp2;
+
+    //constructors
+    property(void);
+
+    // constructor with basic calculation information
+    property(int nbasis, int nmo, int nalpha, int nbeta);
+
+    // construct with hf information
+    property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+             double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_total_energy,\
+             int scf_iterations, int natoms, double *scf_dipole_moment);
+
+    // construct with dft information
+    property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+             double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_xc_energy,\
+             double scf_total_energy, int scf_iterations, int natoms, double *scf_dipole_moment);
+
+   // construct with mp2 information
+    property(int nbasis, int nmo, int nalpha, int nbeta, double scf_one_electron_energy,\
+             double scf_two_electron_energy, double nuclear_repulsion_energy, double scf_total_energy, \
+             int scf_iterations, double mp2_correlation_energy, int natoms, double *scf_dipole_moment);
+
+    //destructor;
+    ~property(void);
+
+};
+
+
+
 }
 
+#endif
